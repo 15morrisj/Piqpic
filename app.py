a662157038f58b74ec5f1f2d99d3158c9fe6ef2f
YOur Name <you@example.com>
2020-08-06 12:57:16 +1200

    Added login pages

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route("/")
+
+@app.route("/home")
 def home():
     return render_template('home.html')
+
+@app.route("/")
+def login():
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
 @app.errorhandler(404)
 def error404(e):
     # note that we set the 404 status explicitly
